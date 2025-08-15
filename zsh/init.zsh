3a751f1d073b965af9a01a9de6d8726a97833f4a
Motoya Nonaka <Motoya.Nonaka@ibm.com>
2025-08-15 22:26:09 +0900

    remove echo for debug

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -9,32 +9,26 @@ if [[ ! -o interactive ]]; then
 fi
 
 if [[ -r "$ZDOTDIR/env.zsh" ]];then
-  echo "$ZDOTDIR/env.zsh is loaded" 
   source "$ZDOTDIR/env.zsh" 
 fi
 
 if [[ -r "$ZDOTDIR/omz.zsh" ]]; then
-  echo "$ZDOTDIR/omz.zsh is loaded" 
   source "$ZDOTDIR/omz.zsh"
 fi
 
 if [[ -r "$ZDOTDIR/aliases.zsh" ]]; then
-  echo "$ZDOTDIR/aliases.zsh is loaded" 
   source "$ZDOTDIR/aliases.zsh"
 fi
 
 if [[ -r "$ZDOTDIR/tools/fzf.zsh" ]]; then
-  echo "$ZDOTDIR/tools/fzf.zsh is loaded" 
   source "$ZDOTDIR/tools/fzf.zsh"
 fi
 
 if [[ -r "$ZDOTDIR/tools/yazi.zsh" ]]; then
-  echo "$ZDOTDIR/tools/yazi.zsh is loaded" 
   source "$ZDOTDIR/tools/yazi.zsh"
 fi
 
 if [[ -r "$ZDOTDIR/tools/iterm2.zsh" ]]; then
-  echo "$ZDOTDIR/tools/iterm2.zsh is loaded" 
   source "$ZDOTDIR/tools/iterm2.zsh"
 fi
 
