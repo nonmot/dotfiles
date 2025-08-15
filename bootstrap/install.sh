bc377512e651d59ce4d049f40d46d60423fb8f60
Motoya Nonaka <Motoya.Nonaka@ibm.com>
2025-08-15 22:25:19 +0900

    bootstrap nvim

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -2,9 +2,16 @@
 set -euo pipefail
 
 DOT="$HOME/dotfiles"
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+
 mkdir -p "$HOME/.config/zsh"
 
+# zsh
 ln -snf "$DOT/zsh/zshrc" "$HOME/.zshrc"
 ln -snf "$DOT/zsh"/* "$HOME/.config/zsh"
+
+# nvim
+mkdir -p "$XDG_CONFIG_HOME"
+ln -snf "$DOT/nvim" "$XDG_CONFIG_HOME/nvim"
 
 echo "Done."
