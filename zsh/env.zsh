95c138425de45d74b751f957e541484f114c0ef4
nonmot <nonmot0614@gmail.com>
2025-12-23 00:35:48 +0900

    yazi

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -6,6 +6,9 @@ mkdir -p "${HISTFILE:h}"
 SAVEHIST=100000
 HISTSIZE=100000
 
+# Homebrew
+export PATH="/opt/homebrew/bin:$PATH"
+
 # pyenv
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
